ccfb03a57c094a41925b970867b2d8f0f3b3a822
drishabh109 <96375523+drishabh109@users.noreply.github.com>
2022-03-04 19:22:13 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import {useState} from 'react'
 
 import '../styles/App.css';
 
 
-const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
-const App = () => {
 
+
+
+
+
+const App = () => {
+const [data,setData]= useState();
+const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
+const search = (e) =>{
+  setData(e.target.value)
+}
   
   return (
     <div id="main">
-      
+    <h4>{searchArray}</h4>
+    <h4>{data}</h4>
+    <h3>search bar</h3>
+      <input type="input" value={search} />
     </div>
   )
 }
